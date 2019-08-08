9460b0eb708893d1b77ae7bd778d55127aff5972
markwh <mark.hagemann@gmail.com>
2019-08-08 09:27:32 -0400

    added example dataset

---
+++ R/datasets-doc.R
@@ -0,0 +1,8 @@
+# Documentation for included datasets
+
+#' #' Sacramento River simulated SWOT data
+#' #'
+#' #' A netcdf containing simulated node and reach-level data from the
+#' #'  Sacramento River, including width, height, and slope
+#' #'
+#' "sacriver"
